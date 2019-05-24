035ebe95184401ce6531ea48f6b050ac89e2114c
Raghu Betina <raghu@firstdraft.com>
2019-05-24 22:02:11 +0000

    Whitelist columns in RoutineProduct for ActiveAdmin

--- app/admin/routine_products.rb
+++ app/admin/routine_products.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register RoutineProduct do
+
+ permit_params :routine_id, :product_id, :use_frequency_id, :note
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
